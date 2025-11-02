0f4cd7cc34201f97d450d4ecae2b0d852f0394d6
rilUnreal <ril_05@mail.ru>
2025-11-02 23:58:35 +0300

    update 2

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 @app.route('/')
 def main():
-    return """
-<h1>Hello</h1>
-<p>Илюша 16 годиков =) В.С.Ё</p>
-<img style="text-align:center" src="https://www.seekpng.com/png/detail/893-8937263_hug.png">
-<a src="youtube.com">
-"""
+    return render_template('index.html')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
