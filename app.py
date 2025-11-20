2885f3298667b98fe4757f25cc9e6f48cc122e1a
rilUnreal <ril_05@mail.ru>
2025-11-20 19:23:11 +0300

    update 4

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+currency_rates = {
+    "USD": 80.49,
+    "KZT": 0.15,
+    "RUB": 1.0,
+}
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/")
+def converter():
+    form_data = dict(request.args)
+    result = 0
+    conversion_text = ""
+
+    if len(form_data) > 0:
+        amount = float(form_data["summ"])
+        from_currency = form_data["from_currency"]
+        to_currency = form_data["to_currency"]
+
+        amount_in_rub = amount * currency_rates[from_currency]
+        result = amount_in_rub / currency_rates[to_currency]
+
+        conversion_text = f"{amount} {from_currency} = {round(result, 2)} {to_currency}"
+
+    return render_template(
+        "index.html", data={"result": result, "conversion_text": conversion_text}
+    )
+
+
+app.run()
